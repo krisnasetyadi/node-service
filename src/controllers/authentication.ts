a1004d1ed629e9d390e9bb8628c001306c2aa108
krisnasetyadi <krisnadwisetyadi@gmail.com>
2023-04-13 20:37:48 +0700

    auth register and login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,35 @@
 import express from 'express'
-import { createUser, getUsersByEmail } from 'db/users'
-import { authentication, random } from 'helpers'
+import { createUser, getUsersByEmail } from '../db/users'
+import { authentication, random } from '../helpers'
+
+export const login = async(req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body
+
+        if(!email || !password) {
+            return res.sendStatus(400)
+        }
+        const user = await getUsersByEmail(email).select('+authentication.salt +authentication.password')
+        if(!user){
+            return res.sendStatus(400)
+        }
+        const expecteedHash = authentication(user.authentication.salt, password)
+
+        if (user.authentication.password !== expecteedHash ){
+            return res.sendStatus(403)
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+        await  user.save()
+        res.cookie('kris-auth', user.authentication.sessionToken, { domain: 'localhost', path: '/' })
+        return res.status(200).json(user).end()
+    } catch (error) {
+        console.log('login error', error)
+        return res.sendStatus(400)
+    }
+}
 
 export const register = async(req: express.Request, res: express.Response ) => {
     try {
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import router from './router'
 
 const app = express()
 
@@ -22,8 +23,10 @@ server.listen(8080, () => {
     console.log('Server running on http://localhost:8080')
 })
 
-const MONGO_URL = 'mongodb+srv://krisnadwisetyadi:z4KReX3deggkcICs@cluster0.ujiykmz.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = ""
 
 mongoose.Promise = Promise
 mongoose.connect(MONGO_URL)
 mongoose.connection.on('error', (error: Error) => console.log('error', error))
+
+app.use('/', router())
