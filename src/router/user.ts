00ff246caf645173aecb04f5cbb6b7321efcc2be
krisnasetyadi <krisnadwisetyadi@gmail.com>
2023-04-13 21:10:45 +0700

    add user route

---
+++ src/router/user.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+import { getAllUsers } from '../controllers/users'
+import { isAuthenticated } from '../middleware'
+export default (router: express.Router) => {
+    router.get('/users', isAuthenticated, getAllUsers)
+}
